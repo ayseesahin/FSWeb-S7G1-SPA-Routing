a317257ceda5630b8e1012fceb53588080069a3e
Ayşe Nur Şahin <aysesah-95@hotmail.com>
2023-04-03 23:44:06 +0300

    finished

--- client/src/Filmler/Film.js
+++ client/src/Filmler/Film.js
@@ -1,25 +1,31 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useParams, useHistory } from "react-router-dom";
 
 export default function Film(props) {
+  const history = useHistory();
   const [movie, setMovie] = useState();
 
-  let id = 1;
+  let { id } = useParams();
+  const { save } = props;
   // URL'den alınan :id parametresini bu değişkene aktarın
+  console.log(id);
 
   useEffect(() => {
     axios
       .get(`http://localhost:5001/api/filmler/${id}`) // Bu uç noktayı Postman'le çalışın
-      .then(response => {
-          // Bu kısmı log statementlarıyla çalışın
-          // ve burdan gelen response'u 'movie' e aktarın
+      .then((response) => {
+        // Bu kısmı log statementlarıyla çalışın
+        // ve burdan gelen response'u 'movie' e aktarın
+        console.log("Film.js", response.data);
+        setMovie(response.data);
       })
-      .catch(error => {
+      .catch((error) => {
         console.error(error);
       });
     // Bu effect her `id ` değiştiğinde çalışmalı
     // Bunu nasıl gerçekleştirebiliriz?
-  }, []);
+  }, [id]);
 
   // Yalnızca esnek görevlere geçtiğinizde burdaki yorum etiketini kaldırın
   // const filmiKaydet = evt => { }
@@ -27,6 +33,11 @@ export default function Film(props) {
   if (!movie) {
     return <div>Film bilgisi yükleniyor...</div>;
   }
+
+  const saveAction = () => {
+    save(id);
+    history.push("/");
+  };
 
   const { title, director, metascore, stars } = movie;
 
@@ -42,13 +53,15 @@ export default function Film(props) {
         </div>
         <h3>Actors</h3>
 
-        {stars.map(star => (
+        {stars.map((star) => (
           <div key={star} className="movie-star">
             {star}
           </div>
         ))}
       </div>
-      <div className="save-button">Kaydet</div>
+      <div onClick={() => saveAction(id)} className="save-button">
+        Kaydet
+      </div>
     </div>
   );
 }
